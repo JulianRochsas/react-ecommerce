3a5f7529290b520325e24cf46a266b77669b97b2
julian_rochsas <fainanda.lead@gmail.com>
2020-03-02 16:09:51 +0700

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    text-transform: capitalize;
+    font-size: 1.4rem;
+    background: transparent;
+    color: var(--mainBlue);
+    border: 1px solid var(--mainBlue);
+    border-radius: 10px;
+    &:hover{
+        background: var(--lighBlue);
+        color: var(--mainBlue);
+    }
+`;
